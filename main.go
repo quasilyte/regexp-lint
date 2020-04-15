d31a66772eeab15d4e13101fb9a28cc3a066c85b
Iskander Sharipov <quasilyte@gmail.com>
2020-04-15 19:59:14 +0300

    add regexp simplifier
    Signed-off-by: Iskander Sharipov <quasilyte@gmail.com>

--- main.go
+++ main.go
@@ -1,19 +1,24 @@
 package main
 
 import (
+	"errors"
+	"strings"
 	"syscall/js"
+
+	gosyntax "regexp/syntax"
 
 	"github.com/quasilyte/regex/syntax"
 )
 
-type lintResult struct {
-	Messages []string
-	Err      string
-}
-
 var vet = &regexpVet{
 	parser: syntax.NewParser(&syntax.ParserOptions{
 		NoLiterals: false,
+	}),
+}
+
+var simplifier = &regexpSimplifier{
+	parser: syntax.NewParser(&syntax.ParserOptions{
+		NoLiterals: true,
 	}),
 }
 
@@ -28,17 +33,48 @@ func stringSliceToInterfaceSlice(prefix string, s []string) []interface{} {
 	return out
 }
 
+func jsError(err error) interface{} {
+	return map[string]interface{}{
+		"Err": err.Error(),
+	}
+}
+
 func regexpLint(this js.Value, args []js.Value) interface{} {
-	// lang := args[0].String()
-	pattern := args[1].String()
-	warnings, err := vet.CheckPattern(pattern)
-	if err != nil {
-		return map[string]interface{}{
-			"Err": err.Error(),
+	pattern := args[0].String()
+	lang := args[1].Get("lang").String()
+	errorsEnables := args[1].Get("errors").Bool()
+	warningsEnabled := args[1].Get("warnings").Bool()
+	suggestionsEnables := args[1].Get("suggestions").Bool()
+
+	if lang != "go" {
+		return jsError(errors.New("unsupported language " + lang))
+	}
+
+	var out []interface{}
+	if errorsEnables {
+		_, err := gosyntax.Parse(pattern, gosyntax.Perl)
+		if err != nil {
+			errMessage := strings.TrimPrefix(err.Error(), "error parsing regexp: ")
+			out = append(out, "error: "+errMessage)
 		}
 	}
+	if warningsEnabled {
+		warnings, err := vet.CheckPattern(pattern)
+		if err != nil {
+			return jsError(err)
+		}
+		out = append(out, stringSliceToInterfaceSlice("warning: ", warnings)...)
+	}
+	if suggestionsEnables {
+		suggestions, err := simplifier.CheckPattern(pattern)
+		if err != nil {
+			return jsError(err)
+		}
+		out = append(out, stringSliceToInterfaceSlice("suggestion: ", suggestions)...)
+	}
+
 	return map[string]interface{}{
-		"Messages": stringSliceToInterfaceSlice("warning: ", warnings),
+		"Messages": out,
 	}
 }
 
---
+++ regexpSimplifier.go
@@ -0,0 +1,330 @@
+package main
+
+import (
+	"strings"
+
+	"github.com/quasilyte/regex/syntax"
+)
+
+type regexpSimplifier struct {
+	parser *syntax.Parser
+
+	suggestions []string
+}
+
+func (c *regexpSimplifier) CheckPattern(pat string) ([]string, error) {
+	re, err := c.parser.Parse(pat)
+	if err != nil {
+		return nil, err
+	}
+
+	c.suggestions = c.suggestions[:0]
+
+	// TODO(quasilyte): suggest char ranges for things like [012345689]?
+	// TODO(quasilyte): evaluate char range to suggest better replacements.
+	// TODO(quasilyte): (?:ab|ac) -> a[bc]
+	// TODO(quasilyte): suggest "s" and "." flag if things like [\w\W] are used.
+	// TODO(quasilyte): do more than 1 round, simplify [^0-9] -> \d.
+	// TODO(quasilyte): x{n}x? -> x{n,n+1}
+
+	c.walk(re.Expr)
+	return c.suggestions, nil
+}
+
+func (c *regexpSimplifier) walk(e syntax.Expr) {
+	switch e.Op {
+	case syntax.OpConcat:
+		c.walkConcat(e)
+
+	case syntax.OpAlt:
+		c.walkAlt(e)
+
+	case syntax.OpCharRange:
+		s := c.simplifyCharRange(e)
+		if s != "" {
+			c.suggestRewrite(e.Value, s)
+		}
+
+	case syntax.OpGroupWithFlags:
+		c.walk(e.Args[0])
+	case syntax.OpGroup:
+		c.walkGroup(e)
+	case syntax.OpCapture:
+		c.walk(e.Args[0])
+	case syntax.OpNamedCapture:
+		c.walk(e.Args[0])
+
+	case syntax.OpRepeat:
+		// TODO(quasilyte): is it worth it to analyze repeat argument
+		// more closely and handle `{n,n} -> {n}` cases?
+		rep := e.Args[1].Value
+		switch rep {
+		case "{0,1}":
+			c.suggestRewrite(e.Value, "%s?", e.Args[0].Value)
+		case "{1,}":
+			c.suggestRewrite(e.Value, "%s+", e.Args[0].Value)
+		case "{0,}":
+			c.suggestRewrite(e.Value, "%s*", e.Args[0].Value)
+		case "{0}":
+			c.suggestRewrite(e.Value, "")
+		case "{1}":
+			c.suggestRewrite(e.Value, e.Args[0].Value)
+		default:
+			c.walk(e.Args[0])
+		}
+
+	case syntax.OpNegCharClass:
+		s := c.simplifyNegCharClass(e)
+		if s != "" {
+			c.suggestRewrite(e.Value, s)
+		} else {
+			for _, a := range e.Args {
+				c.walk(a)
+			}
+		}
+
+	case syntax.OpCharClass:
+		s := c.simplifyCharClass(e)
+		if s != "" {
+			c.suggestRewrite(e.Value, s)
+		} else {
+			for _, a := range e.Args {
+				c.walk(a)
+			}
+		}
+
+	case syntax.OpEscapeChar:
+		switch e.Value {
+		case `\&`, `\#`, `\!`, `\@`, `\%`, `\<`, `\>`, `\:`, `\;`, `\/`, `\,`, `\=`, `\.`:
+			c.suggestRewrite(e.Value, e.Value[len(`\`):])
+		}
+
+	case syntax.OpQuestion, syntax.OpNonGreedy:
+		c.walk(e.Args[0])
+	case syntax.OpStar:
+		c.walk(e.Args[0])
+	case syntax.OpPlus:
+		c.walk(e.Args[0])
+
+	default:
+		for _, a := range e.Args {
+			c.walk(a)
+		}
+	}
+}
+
+func (c *regexpSimplifier) walkGroup(g syntax.Expr) {
+	switch g.Args[0].Op {
+	case syntax.OpChar, syntax.OpEscapeChar, syntax.OpEscapeMeta, syntax.OpCharClass:
+		c.suggestRewrite(g.Value, g.Args[0].Value)
+	}
+
+	c.walk(g.Args[0])
+}
+
+func (c *regexpSimplifier) simplifyNegCharClass(e syntax.Expr) string {
+	switch e.Value {
+	case `[^0-9]`:
+		return `\D`
+	case `[^\s]`:
+		return `\S`
+	case `[^\S]`:
+		return `\s`
+	case `[^\w]`:
+		return `\W`
+	case `[^\W]`:
+		return `\w`
+	case `[^\d]`:
+		return `\D`
+	case `[^\D]`:
+		return `\d`
+	case `[^[:^space:]]`:
+		return `\s`
+	case `[^[:space:]]`:
+		return `\S`
+	case `[^[:^word:]]`:
+		return `\w`
+	case `[^[:word:]]`:
+		return `\W`
+	case `[^[:^digit:]]`:
+		return `\d`
+	case `[^[:digit:]]`:
+		return `\D`
+	}
+
+	return ""
+}
+
+func (c *regexpSimplifier) simplifyCharClass(e syntax.Expr) string {
+	switch e.Value {
+	case `[0-9]`:
+		return `\d`
+	case `[[:word:]]`:
+		return `\w`
+	case `[[:^word:]]`:
+		return `\W`
+	case `[[:digit:]]`:
+		return `\d`
+	case `[[:^digit:]]`:
+		return `\D`
+	case `[[:space:]]`:
+		return `\s`
+	case `[[:^space:]]`:
+		return `\S`
+	case `[][]`:
+		return `\]\[`
+	case `[]]`:
+		return `\]`
+	}
+
+	if len(e.Args) == 1 {
+		switch e.Args[0].Op {
+		case syntax.OpChar:
+			switch v := e.Args[0].Value; v {
+			case "|", "*", "+", "?", ".", "[", "^", "$", "(", ")":
+				// Can't take outside of the char group without escaping.
+			default:
+				return v
+			}
+		case syntax.OpEscapeChar:
+			return e.Args[0].Value
+		}
+	}
+
+	return ""
+}
+
+func (c *regexpSimplifier) canMerge(x, y syntax.Expr) bool {
+	if x.Op != y.Op {
+		return false
+	}
+	switch x.Op {
+	case syntax.OpChar, syntax.OpCharClass, syntax.OpEscapeMeta, syntax.OpEscapeChar, syntax.OpNegCharClass, syntax.OpGroup:
+		return x.Value == y.Value
+	default:
+		return false
+	}
+}
+
+func (c *regexpSimplifier) canCombine(x, y syntax.Expr) (threshold int, ok bool) {
+	if x.Op != y.Op {
+		return 0, false
+	}
+
+	switch x.Op {
+	case syntax.OpDot:
+		return 3, true
+
+	case syntax.OpChar:
+		if x.Value != y.Value {
+			return 0, false
+		}
+		if x.Value == " " {
+			return 1, true
+		}
+		return 4, true
+
+	case syntax.OpEscapeMeta, syntax.OpEscapeChar:
+		if x.Value == y.Value {
+			return 2, true
+		}
+
+	case syntax.OpCharClass, syntax.OpNegCharClass, syntax.OpGroup:
+		if x.Value == y.Value {
+			return 1, true
+		}
+	}
+
+	return 0, false
+}
+
+func (c *regexpSimplifier) walkAlt(alt syntax.Expr) {
+	allChars := true
+	for _, e := range alt.Args {
+		if e.Op != syntax.OpChar {
+			allChars = false
+			break
+		}
+	}
+
+	if allChars {
+		var b strings.Builder
+		b.WriteString("[")
+		for _, e := range alt.Args {
+			b.WriteString(e.Value)
+		}
+		b.WriteString("]")
+		c.suggestRewrite(alt.Value, b.String())
+	} else {
+		for _, a := range alt.Args {
+			c.walk(a)
+		}
+	}
+}
+
+func (c *regexpSimplifier) walkConcat(concat syntax.Expr) {
+	i := 0
+	for i < len(concat.Args) {
+		x := concat.Args[i]
+		c.walk(x)
+		i++
+
+		if i >= len(concat.Args) {
+			break
+		}
+
+		// Try merging `xy*` into `x+` where x=y.
+		if concat.Args[i].Op == syntax.OpStar {
+			if c.canMerge(x, concat.Args[i].Args[0]) {
+				c.suggestRewrite(x.Value+concat.Args[i].Value, "%s+", x.Value)
+				i++
+				continue
+			}
+		}
+
+		// // Try combining `xy` into `x{2}` where x=y.
+		// threshold, ok := c.canCombine(x, concat.Args[i])
+		// if !ok {
+		// 	continue
+		// }
+		// n := 1 // Can combine at least 1 pair.
+		// for j := i + 1; j < len(concat.Args); j++ {
+		// 	_, ok := c.canCombine(x, concat.Args[j])
+		// 	if !ok {
+		// 		break
+		// 	}
+		// 	n++
+		// }
+		// if n >= threshold {
+		// 	fmt.Fprintf(c.out, "{%d}", n+1)
+		// 	c.score++
+		// 	i += n
+		// }
+	}
+}
+
+func (c *regexpSimplifier) simplifyCharRange(rng syntax.Expr) string {
+	if rng.Args[0].Op != syntax.OpChar || rng.Args[1].Op != syntax.OpChar {
+		return ""
+	}
+
+	lo := rng.Args[0].Value
+	hi := rng.Args[1].Value
+	if len(lo) == 1 && len(hi) == 1 {
+		switch hi[0] - lo[0] {
+		case 0:
+			return lo
+		case 1:
+			return lo + hi
+		case 2:
+			return lo + string(lo[0]+1) + hi
+		}
+	}
+
+	return ""
+}
+
+func (c *regexpSimplifier) suggestRewrite(orig, format string, args ...interface{}) {
+	suggestion := "`" + orig + "` -> `" + sprintf(format, args...) + "`"
+	c.suggestions = append(c.suggestions, suggestion)
+}
